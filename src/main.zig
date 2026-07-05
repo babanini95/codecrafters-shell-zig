a8b49f92a81ee326f96fc9e937a7f85cc8184cd1
Bani <69472457+babanini95@users.noreply.github.com>
2026-07-06 06:56:46 +0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -6,13 +6,15 @@ pub fn main(init: std.process.Init) !void {
     // Suppress unused local constant error. Feel free to remove the line below.
     try stdout.interface.print("", .{});
 
-    // TODO: Uncomment the code below to pass the first stage
-    try stdout.interface.print("$ ", .{});
+    while (true) {
+        // TODO: Uncomment the code below to pass the first stage
+        try stdout.interface.print("$ ", .{});
 
-    var stdin_buffer: [4096]u8 = undefined;
-    var stdin = std.Io.File.stdin().readerStreaming(init.io, &stdin_buffer);
+        var stdin_buffer: [4096]u8 = undefined;
+        var stdin = std.Io.File.stdin().readerStreaming(init.io, &stdin_buffer);
 
-    const command = try stdin.interface.takeDelimiter('\n');
+        const command = try stdin.interface.takeDelimiter('\r');
 
-    try stdout.interface.print("{s}: command not found\n", .{command.?});
+        try stdout.interface.print("asds{s}: command not found\n", .{command.?});
+    }
 }
