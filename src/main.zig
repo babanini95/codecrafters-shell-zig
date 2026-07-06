6b9f5b09f6dd919a9ed629258d6595de51cdf2f7
Bani <69472457+babanini95@users.noreply.github.com>
2026-07-06 11:12:05 +0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -5,13 +5,16 @@ pub fn main(init: std.process.Init) !void {
     var stdin_buffer: [4096]u8 = undefined;
     var stdin = std.Io.File.stdin().readerStreaming(init.io, &stdin_buffer);
 
-    // Suppress unused local constant error. Feel free to remove the line below.
     try stdout.interface.print("", .{});
 
     while (true) {
         try stdout.interface.print("$ ", .{});
 
         const command = try stdin.interface.takeDelimiter('\n');
+
+        if (std.mem.eql(u8, command.?, "exit")) {
+            break;
+        }
 
         try stdout.interface.print("{s}: command not found\n", .{command.?});
     }
