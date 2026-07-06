35ed35183b24b1ecf6d1151959c064cc0c68e7b2
Bani <69472457+babanini95@users.noreply.github.com>
2026-07-06 08:51:37 +0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -2,16 +2,14 @@ const std = @import("std");
 
 pub fn main(init: std.process.Init) !void {
     var stdout = std.Io.File.stdout().writer(init.io, &.{});
+    var stdin_buffer: [4096]u8 = undefined;
+    var stdin = std.Io.File.stdin().readerStreaming(init.io, &stdin_buffer);
 
     // Suppress unused local constant error. Feel free to remove the line below.
     try stdout.interface.print("", .{});
 
     while (true) {
-        // TODO: Uncomment the code below to pass the first stage
         try stdout.interface.print("$ ", .{});
-
-        var stdin_buffer: [4096]u8 = undefined;
-        var stdin = std.Io.File.stdin().readerStreaming(init.io, &stdin_buffer);
 
         const command = try stdin.interface.takeDelimiter('\n');
 
