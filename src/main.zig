0dba8258fd6c852f9c50460b277aed541d3ea6cc
Bani <69472457+babanini95@users.noreply.github.com>
2026-07-06 12:04:22 +0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -10,14 +10,15 @@ pub fn main(init: std.process.Init) !void {
     while (true) {
         try stdout.interface.print("$ ", .{});
 
-        const command = try stdin.interface.takeDelimiter('\n');
+        const bare_line = try stdin.interface.takeDelimiter('\n');
+        const command = std.mem.trim(u8, bare_line.?, "\r");
 
-        if (std.mem.eql(u8, command.?, "exit")) {
+        if (std.mem.eql(u8, command, "exit")) {
             break;
-        } else if (std.mem.startsWith(u8, command.?, "echo")) {
-            try stdout.interface.print("{s}\n", .{command.?[5..]});
+        } else if (std.mem.startsWith(u8, command, "echo")) {
+            try stdout.interface.print("{s}\n", .{command[5..]});
         } else {
-            try stdout.interface.print("{s}: command not found\n", .{command.?});
+            try stdout.interface.print("{s}: command not found\n", .{command});
         }
     }
 }
