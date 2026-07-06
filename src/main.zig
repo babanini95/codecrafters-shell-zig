1f753f33dfd81c80318c6876264c520c37d25fd6
Bani <69472457+babanini95@users.noreply.github.com>
2026-07-06 13:27:03 +0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+const Commands = enum { exit, echo, type };
+
 pub fn main(init: std.process.Init) !void {
     var stdout = std.Io.File.stdout().writer(init.io, &.{});
     var stdin_buffer: [4096]u8 = undefined;
@@ -10,13 +12,22 @@ pub fn main(init: std.process.Init) !void {
     while (true) {
         try stdout.interface.print("$ ", .{});
 
-        const bare_line = try stdin.interface.takeDelimiter('\n');
-        const command = std.mem.trim(u8, bare_line.?, "\r");
+        const line = try stdin.interface.takeDelimiter('\n');
+        var t_command = std.mem.tokenizeAny(u8, std.mem.trim(u8, line.?, "\r"), " \t");
+        const command = t_command.next().?;
+        const args = t_command.rest();
 
         if (std.mem.eql(u8, command, "exit")) {
             break;
-        } else if (std.mem.startsWith(u8, command, "echo")) {
+        } else if (std.mem.eql(u8, command, "echo")) {
             try stdout.interface.print("{s}\n", .{command[5..]});
+        } else if (std.mem.eql(u8, command, "type")) {
+            const com = std.meta.stringToEnum(Commands, args) orelse {
+                try stdout.interface.print("{s}: not found\n", .{args});
+                continue;
+            };
+
+            try stdout.interface.print("{s} is a shell builtin\n", .{std.enums.tagName(Commands, com).?});
         } else {
             try stdout.interface.print("{s}: command not found\n", .{command});
         }
