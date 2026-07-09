a4829a9c80e160144599013d02763fa9f6801dde
Bani <69472457+babanini95@users.noreply.github.com>
2026-07-09 13:52:49 +0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -17,8 +17,9 @@ pub fn main(init: std.process.Init) !void {
     var stdin_buffer: [4096]u8 = undefined;
     var stdin = std.Io.File.stdin().readerStreaming(init.io, &stdin_buffer);
 
+    const is_windows = builtin.os.tag == .windows;
     const path = init.environ_map.get("PATH").?;
-    const path_sep: u8 = if (builtin.os.tag == .windows) ';' else ':';
+    const path_sep: u8 = if (is_windows) ';' else ':';
 
     try stdout.interface.print("", .{});
 
@@ -45,17 +46,20 @@ pub fn main(init: std.process.Init) !void {
                     var path_iterator = std.mem.splitScalar(u8, path, path_sep);
 
                     const extensions: []const []const u8 =
-                        if (builtin.os.tag == .windows)
+                        if (is_windows)
                             &.{ "", ".exe", ".cmd", ".bat", ".com" }
                         else
                             &.{""};
 
                     search: while (path_iterator.next()) |directory| {
                         for (extensions) |ext| {
-                            const candidate = try std.mem.concat(allocator, u8, &.{ args, ext });
-                            const full_path = try std.fs.path.join(allocator, &.{ directory, candidate });
+                            const candidate = std.mem.concat(allocator, u8, &.{ args, ext }) catch continue;
+                            const full_path = std.fs.path.join(allocator, &.{ directory, candidate }) catch continue;
 
-                            std.Io.Dir.cwd().access(init.io, full_path, .{}) catch continue;
+                            const file_stat = std.Io.Dir.cwd().statFile(init.io, full_path, .{}) catch continue;
+                            try stdout.interface.print("{any}\n", .{file_stat.permissions});
+                            // if (!can_execute) continue;
+                            // std.Io.Dir.cwd().access(init.io, full_path, .{}) catch continue;
 
                             filepath = try allocator.dupe(u8, full_path);
                             break :search;
