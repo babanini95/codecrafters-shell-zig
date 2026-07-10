a360466d94d1ce54d967e22a5ffe638d03fc41fa
Bani <69472457+babanini95@users.noreply.github.com>
2026-07-10 16:17:30 +0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -17,8 +17,11 @@ pub fn main(init: std.process.Init) !void {
     while (true) {
         try stdout.interface.print("$ ", .{});
 
-        const line = try stdin.interface.takeDelimiter('\n');
-        const trimmed = std.mem.trim(u8, line.?, "\r");
+        const line = try stdin.interface.takeDelimiter('\n') orelse "";
+        const trimmed = if (builtin.os.tag == .windows)
+            std.mem.trim(u8, line, "\r")
+        else
+            line;
         var t_command = std.mem.tokenizeAny(u8, trimmed, " \t");
         const command_str = t_command.next() orelse "";
         const command = Commands.fromString(command_str) orelse .invalid;
